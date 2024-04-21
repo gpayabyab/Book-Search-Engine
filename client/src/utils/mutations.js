7f759adc11c95375feaed46da4cab4d4adf7cd06
Gabrielle 🌿 <gabrielle@gabrieleslaptop.mynetworksettings.com>
2024-04-20 21:34:15 -0400

    added code to quieries.js, mutations.js, and replaced proxy in vite.config.js

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,37 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(userName: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql``;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(skill: $bookId) {
+      _id
+      name
+      skills
+    }
+  }
+`;
